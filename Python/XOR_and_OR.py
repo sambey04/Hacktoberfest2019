0a976044cacb12096fcc0e083ca977f8783c89e1
Ezequias Rocha <44200063+ezequiasOR@users.noreply.github.com>
2019-10-14 13:10:54 -0300

    Create XOR_and_OR.py
    Codeforces code solution

---
+++ Python/XOR_and_OR.py
@@ -0,0 +1,10 @@
+Codeforces: problem's  solution XOR and OR
+https://codeforces.com/contest/282/problem/C
+
+a = input()
+b = input()
+ 
+if a!=b and (len(a)!=len(b) or a.count('0') == len(a) or b.count('0') == len(b)):
+    print('NO')
+else:
+    print('YES')
