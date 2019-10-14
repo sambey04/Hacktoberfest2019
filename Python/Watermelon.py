208beb5c6b631133ea2aeae2466f704119ebc40c
Ezequias Rocha <44200063+ezequiasOR@users.noreply.github.com>
2019-10-14 13:15:27 -0300

    Create Watermelon.py
    Codeforces code solution

---
+++ Python/Watermelon.py
@@ -0,0 +1,12 @@
+# Codeforces: problem's  solution XOR and OR
+# https://codeforces.com/contest/4/problem/A
+# Python 2.7
+
+#coding: utf8
+ 
+w = int(raw_input())
+ 
+if (w-2)%2==0 and (w-2)!=0:
+	print "YES"
+else:
+	print "NO"
