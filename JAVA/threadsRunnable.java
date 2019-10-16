6b7130aee6ea65759b83a017e5190289c9e4905c
Meetzanonymous <30690468+Meetzanonymous@users.noreply.github.com>
2019-10-16 17:07:05 +0530

    magic_no.cpp
    Cpp Code to Find the number is Magic or Not

--- JAVA/threadsRunnable.java
+++ JAVA/threadsRunnable.java
@@ -1,11 +1,14 @@
 import java.io.*;
-public class nsp{
-	public static void main(String[]args){
-		Thread t=new Thread(){
-			public void run(){
-				System.out.println("java is ");
-			}
-		};
-			t.start();	
+class demo implements Runnable{
+	public void run(){
+		System.out.println("thread is ");
+	}
 }
+public class nsn{
+
+	public static void main(String[]args){
+		demo obj=new demo();
+		Thread(t)=new Thread(obj);
+		t.start();
+	}
 }
