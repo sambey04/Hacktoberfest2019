5fef2dd1da92143e830017878f332d0a856a68c4
Pedro Aaron <p.luis.aaron@gmail.com>
2019-10-15 22:49:40 -0500

    Add L1 Numpy Example

--- Python/numpyexample.py
+++ Python/numpyexample.py
@@ -5,4 +5,14 @@ print(np.__version__)
 np.array([1, 4, 2, 5, 3])
 #float array
 np.array([1, 2, 3, 4], dtype='float32')
-
+#nested lists result in multidimensional arrays
+np.array([range(i, i + 3) for i in [2, 4, 6]])
+#Create a 3×3 numpy array of all True’s
+np.ones((3,3),dtype=bool)
+#Extract all odd numbers from arr
+arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+arr[arr % 2 == 1]
+#Replace all odd numbers in arr with -1
+arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+arr[arr % 2 == 1] = -1
+arr
